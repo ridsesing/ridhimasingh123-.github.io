295c371cc6832905ca4e185a39746ded44b6984f
ridsesing <114339202+ridsesing@users.noreply.github.com>
2022-09-26 12:10:42 +0530

    Add files via upload
    Signed-off-by: ridsesing <114339202+ridsesing@users.noreply.github.com>

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer","content writer","content creater"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Hire me"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
